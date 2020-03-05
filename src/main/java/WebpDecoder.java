fc3e657ea6f80f2f27e5d5d15e5e4570c310a33e
keshanechen <keshanechen@gmail.com>
2020-03-05 14:56:42 +0800

    update decoder

--- src/main/java/WebpDecoder.java
+++ src/main/java/WebpDecoder.java
@@ -7,9 +7,20 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * @author Keshane
+ * a webp decoder class to decode webp
+ */
 public class WebpDecoder {
 
-    public static void decode(String webpimagePath , String type , String objectImagePath ) throws IOException {
+    /**
+     * default method to encode webp image by default compression param
+     * @param webpImagePath the origin webp image path
+     * @param type the type you want to convert to
+     * @param objectImagePath the object image path
+     * @throws IOException read file may cause IOException
+     */
+    public static void decode(String webpImagePath , String type , String objectImagePath ) throws IOException {
         // Obtain a WebP ImageReader instance
         ImageReader reader = ImageIO.getImageReadersByMIMEType("image/webp").next();
 
@@ -18,7 +29,7 @@ public class WebpDecoder {
         readParam.setBypassFiltering(true);
 
         // Configure the input on the ImageReader
-        reader.setInput(new FileImageInputStream(new File(webpimagePath)));
+        reader.setInput(new FileImageInputStream(new File(webpImagePath)));
 
         // Decode the image
         BufferedImage image = reader.read(0, readParam);
